0de55112e8e6e9e8aad596574364355e05839102
Arjunn-Arora <92421448+Arjunn-Arora@users.noreply.github.com>
2025-05-30 15:55:32 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { assets } from "../assets/assets";
-import { AppContent } from "../context/AppContext";
+import { AppContext } from "../context/AppContext";
 
 export const Header = () => {
 
