7316141644b9dd58dfdaf8042cba277135458a14
Arjunn-Arora <92421448+Arjunn-Arora@users.noreply.github.com>
2025-05-30 14:04:03 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -8,7 +8,7 @@ export const AppContentProvider = (props) => {
 
     axios.defaults.withCredentials = true;
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const backendUrl = "https://secureauth-backend-msml.onrender.com";
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [userData, setUserData] = useState(false);
 
